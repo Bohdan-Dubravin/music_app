f9f6168ab7921d265afad4a4b12812a641082acc
daria-pro <dubravin111@gmail.com>
2023-01-24 17:57:50 +0200

    songs list

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/Home.vue';
-import About from '@/views/About.vue';
-import Manage from '@/views/Manage.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
+import Song from '@/views/Song.vue'
 
 const routes = [
   { path: '/', component: Home },
@@ -14,13 +15,15 @@ const routes = [
       requiresAuth: true,
     },
   },
+
+  { name: 'song', path: '/song/:id', component: Song },
   { path: '/:catchAll(.*)*', redirect: '/' },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: 'text-yellow-500',
-});
+})
 
-export default router;
+export default router
