48a9868d5b2aebe5bad1c727e9051993600f0433
Bohdan-Dubravin <dubravin111@gmail.com>
2023-01-26 22:42:09 +0200

    player bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,44 +1,67 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-import { Howl } from 'howler'
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { Howl } from 'howler';
+import timeFormat from '@/utils/timeFormat';
 
 export const usePlayerStore = defineStore('player', () => {
-  const currentSong = ref({})
-  const sound = ref({})
-  const seek = ref('00:00')
-  const duration = ref('00:00')
+  const currentSong = ref({});
+  const sound = ref({});
+  const seek = ref('00:00');
+  const duration = ref('00:00');
+  const playerProgress = ref('0%');
 
   const newSong = async (song) => {
-    currentSong.value = song
+    if (sound.value instanceof Howl) {
+      sound.value.unload();
+    }
 
-    sound.value = new Howl({ src: [song.url], html5: true })
-    console.log(sound.value)
-    sound.value.play()
+    currentSong.value = song;
+
+    sound.value = new Howl({ src: [song.url], html5: true });
+    sound.value.play();
     sound.value.on('play', () => {
-      requestAnimationFrame(progress.value)
-    })
-  }
+      requestAnimationFrame(progress);
+    });
+  };
 
   const toggleAudio = async () => {
     if (!sound.value.playing) {
-      return
+      return;
     }
     if (sound.value.playing()) {
-      sound.value.pause()
+      sound.value.pause();
     } else {
-      sound.value.play()
+      sound.value.play();
     }
-  }
+  };
   const progress = () => {
-    seek.value === sound.value.seek()
-  }
+    seek.value = timeFormat.formatTime(sound.value.seek());
+    duration.value = timeFormat.formatTime(sound.value.duration());
+
+    playerProgress.value = `${
+      (sound.value.seek() / sound.value.duration()) * 100
+    }%`;
+
+    if (sound.value.playing()) {
+      requestAnimationFrame(progress);
+    }
+  };
+
   const playing = computed(() => {
     if (sound.value.playing) {
-      return sound.value.playing()
+      return sound.value.playing();
     }
 
-    return false
-  })
+    return false;
+  });
 
-  return { newSong, toggleAudio, playing, seek, duration }
-})
+  return {
+    newSong,
+    toggleAudio,
+    playing,
+    seek,
+    duration,
+    playerProgress,
+    currentSong,
+  };
+});
